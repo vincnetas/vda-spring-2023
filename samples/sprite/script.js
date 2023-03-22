b44f5e2bb121b1f346b8ee35fc04e72808af89f5
Vincentas Vienožinskis <vincnetas@gmail.com>
2023-03-22 17:48:16 +0200

    remove scene

--- samples/sprite/script.js
+++ samples/sprite/script.js
@@ -3,7 +3,7 @@ var config = {
     parent: "phaser-example",
     backgroundColor: "#0055aa",
     width: 800,
-    scene: [Scene2, Scene1],
+    scene: [Scene1],
     height: 600,
     physics: {
       default: 'matter',
